65755190665129caa9e8dbc4b99c66c0bab43ad5
Jay <1995jayhu@gmail.com>
2026-03-29 14:55:55 +0800

    公告跑馬燈修正ok

--- project/script.js
+++ project/script.js
@@ -1,3 +1,24 @@
-// 取得跑馬燈元素
-const announcement = document.querySelector('.annouancement');
-const announcementBox = document.querySelector('.annouancement-box');
+function load() {
+    var list = document.querySelector('.list');
+    var box = document.querySelector('.box');
+    list.innerHTML += list.innerHTML;
+    list.innerHTML += list.innerHTML;
+    let left = 0;
+    let timer;
+
+    function move() {
+        clearInterval(timer);
+        timer = setInterval(() => {
+            left = left - 2;
+            let contentWidth = list.offsetWidth / 2;
+            
+            if (Math.abs(left) >= contentWidth) {
+                left = 0;
+            }
+            list.style.left = left + 'px';
+        }, 20);
+    }
+    move();
+}
+
+document.addEventListener('DOMContentLoaded', load);
